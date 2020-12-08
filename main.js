749e33b6fb7334d7d907a5fb6c388bc1e394f0c3
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2020-12-08 15:29:30 +0100

    riprendo

--- main.js
+++ main.js
@@ -2,10 +2,43 @@ var app = new Vue({
     el: '#root',
 
     data: {
+        navbarData: [
+            {
+                menuTitle: 'Home',
+                hover: false,
+            },
+            {
+                menuTitle: 'About Me',
+                hover: false,
+            },
+            {
+                menuTitle: 'Testimonials',
+                hover: false,
+            },
+            {
+                menuTitle: 'My Blog',
+                hover: false,
+            },
+            {
+                menuTitle: 'Meetups',
+                hover: false,
+            },
+            {
+                menuTitle: 'Shop',
+                hover: false,
+            },
+            {
+                menuTitle: 'Contact Me',
+                hover: false,
+            },
 
+        ],
+
+        showthis: true,
     },
 
     methods: {
-        
+
+
     }
 })
